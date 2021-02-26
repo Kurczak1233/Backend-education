b62f8682372ddf909f0a1e02175780fcf53310c5
Kurczak1233 <kurczak12333@gmail.com>
2021-02-26 17:57:43 +0100

    Add Delete and Update

--- C#/CRUD/CRUD/CRUD/Program.cs
+++ C#/CRUD/CRUD/CRUD/Program.cs
@@ -13,27 +13,31 @@ namespace CRUD
                 connection.Open();
                 //CREATE
                 var CreateCommand = new SqlCommand("INSERT INTO Employees(FirstName, LastName) values(@firstName, @lastName)", connection);
-                CreateCommand.Parameters.AddWithValue("firstName", "Michał");
+                CreateCommand.Parameters.AddWithValue("firstName", "Michał"); //First crucial defend against SQL Injections!
                 CreateCommand.Parameters.AddWithValue("lastName", "Kupczak");
                 var Create = CreateCommand.ExecuteNonQuery();
                 //READ                 
-                var command = new SqlCommand("SELECT COUNT(*) FROM Employees", connection);
-                Console.WriteLine(command.ExecuteScalar());
+                var ReadingCommand = new SqlCommand("SELECT COUNT(*) FROM Employees", connection);
+                Console.WriteLine(ReadingCommand.ExecuteScalar());
 
-                var command2 = new SqlCommand("SELECT TOP(10) o.EmployeeID [Worker's ID], c.CustomerID [Customer's ID] " +
+                var ReadingCommand2 = new SqlCommand("SELECT TOP(10) o.EmployeeID [Worker's ID], c.CustomerID [Customer's ID] " +
                     "FROM Orders o JOIN Customers c ON o.CustomerID = c.CustomerID", connection);
-                var reader = command2.ExecuteReader();
-                while (reader.Read())
+                var Read = ReadingCommand2.ExecuteReader();
+                while (Read.Read())
                 {
                     Console.WriteLine("{0} {1}",
-                       reader[0],
-                       reader[1]);
+                       Read[0],
+                       Read[1]);
                 }
                 //UPDATE
-
+                var UpdateCommand = new SqlCommand("UPDATE Employees SET FirstName=@name WHERE EmployeeId=@Id", connection);
+                UpdateCommand.Parameters.AddWithValue("name", "Kicia");
+                UpdateCommand.Parameters.AddWithValue("id", "10");
+                var Update = UpdateCommand.ExecuteNonQuery();
                 //DELETE
-
-
+                var DeleteCommand = new SqlCommand("DELETE FROM Employees WHERE EmployeeId=@Id", connection);
+                DeleteCommand.Parameters.AddWithValue("Id", "11");
+                var Delete = DeleteCommand.ExecuteNonQuery();
             }
         }
     }
