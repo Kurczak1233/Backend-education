51997590bbf96e6e5536f95649c60ecf2b847652
Kurczak1233 <kurczak12333@gmail.com>
2021-02-26 18:18:27 +0100

    Identity implementation

--- Entity Framework/EntityFramework/EntiryFramework.Database/EntityFrameworkDbContext.cs
+++ Entity Framework/EntityFramework/EntiryFramework.Database/EntityFrameworkDbContext.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace EntiryFramework.Database
 {
-    public class EntityFrameworkDbContext : DbContext
+    public class EntityFrameworkDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Setting> Settings { get; set; }
         public EntityFrameworkDbContext(DbContextOptions options) : base(options)
