d50932e0a63ad087ad4b89a8adf2c89b84cecd5b
Kurczak1233 <kurczak12333@gmail.com>
2021-02-26 20:34:40 +0100

    Testet initial migration - works fine

---
+++ Entity Framework/EntityFramework/EntityFramework/Migrations/20210226193238_AddTestPropToUsers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFramework.Migrations
+{
+    public partial class AddTestPropToUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TESTPROPERTY",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TESTPROPERTY",
+                table: "AspNetUsers");
+        }
+    }
+}
