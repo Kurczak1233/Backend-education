62f503cd53730bc04699120987ae505bfc57520e
Kurczak1233 <kurczak12333@gmail.com>
2021-02-26 18:37:09 +0100

    Added tools (NuGet Package to add migrations)

--- Entity Framework/EntityFramework/EntiryFramework.Database/EntityFrameworkDbContext.cs
+++ Entity Framework/EntityFramework/EntiryFramework.Database/EntityFrameworkDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EntiryFramework.Database
 {
-    public class EntityFrameworkDbContext : IdentityDbContext<ApplicationUser>
+    public class EntityFrameworkDbContext : IdentityDbContext
     {
         public DbSet<Setting> Settings { get; set; }
         public EntityFrameworkDbContext(DbContextOptions options) : base(options)
