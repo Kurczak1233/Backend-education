2a91d301b723e05e96a8bdb88f2b926c25fa3199
Kurczak1233 <kurczak12333@gmail.com>
2021-02-26 19:34:24 +0100

    Updated db after total reset.

--- Entity Framework/EntityFramework/EntiryFramework.Database/ApplicationUser.cs
+++ Entity Framework/EntityFramework/EntiryFramework.Database/ApplicationUser.cs
@@ -4,7 +4,6 @@ namespace EntiryFramework.Database
 {
     public class ApplicationUser : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string City { get; set; }
     }
 }
