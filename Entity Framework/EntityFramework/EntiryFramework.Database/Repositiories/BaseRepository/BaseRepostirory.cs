8ec5b67d6b3fc71ac0285f8741e4a01848a196a4
Kurczak1233 <kurczak12333@gmail.com>
2021-02-27 15:28:19 +0100

    Created base repository

---
+++ Entity Framework/EntityFramework/EntiryFramework.Database/Repositiories/BaseRepository/BaseRepostirory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EntiryFramework.Database.Repositiories.BaseRepository
+{
+    public abstract class BaseRepostirory<Entity> where Entity : class
+    {
+        protected EntityFrameworkDbContext _db;
+        protected abstract DbSet<Entity> DbSet { get; }
+        public BaseRepostirory(EntityFrameworkDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<Entity> GetAll()
+        {
+            var list = new List<Entity>();
+            //var list2 = await _db.Settings.ToListAsync(); Simplier?
+            var entities = DbSet; //Wszystkie settingi się tutaj znajdą.
+            foreach (var entity in entities)
+            {
+                list.Add(entity);
+            }
+            return list;
+        }
+
+        public void SaveChanges()
+        {
+            _db.SaveChangesAsync();
+        }
+    }
+}
+
---
+++ Entity Framework/EntityFramework/EntiryFramework.Database/Repositiories/SettingsRepository.cs
@@ -0,0 +1,32 @@
+﻿using EntiryFramework.Database.Repositiories.BaseRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntiryFramework.Database.Repositiories.SettingsRepository
+{
+    public  class SettingsRepository : BaseRepostirory<Setting>
+    {
+        protected override DbSet<Setting> DbSet => _db.Settings;
+        public SettingsRepository(EntityFrameworkDbContext dbContext) : base(dbContext) { }
+
+        public void UpdateSettings(Setting setting)
+        {
+            var foundSetting = DbSet.Where(c => c.Id == setting.Id).FirstOrDefault();
+            if (foundSetting == null)
+            {
+                DbSet.Add(setting);
+                SaveChanges();
+                return;
+            }
+            else
+            {
+                foundSetting.Name = setting.Name;
+                foundSetting.Value = setting.Value;
+                SaveChanges();
+                return;
+            }
+        }
+    }
+}
