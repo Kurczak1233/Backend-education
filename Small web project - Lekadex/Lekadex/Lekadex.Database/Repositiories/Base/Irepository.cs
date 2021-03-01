76979144c91a4133b15eac3c0d6fffd33bb33d92
Kurczak1233 <kurczak12333@gmail.com>
2021-03-01 22:43:14 +0100

    FixBugs

--- Small web project - Lekadex/Lekadex/Lekadex.Database/Repositiories/Base/Irepository.cs
+++ Small web project - Lekadex/Lekadex/Lekadex.Database/Repositiories/Base/Irepository.cs
@@ -2,7 +2,7 @@
 {
     public interface IRepository<Entity> where Entity : BaseEntity
     {
-        void AddNew(Entity entity);
+        bool AddNew(Entity entity);
 
         bool Delete(Entity entity);
     }
