305a54d94cb9f8ac11b255c571b5002a6bea48c3
Kurczak1233 <kurczak12333@gmail.com>
2021-03-02 12:21:39 +0100

    Create manager

--- Small web project - Lekadex/Lekadex/Lekadex.Core/DoctorManager.cs
+++ Small web project - Lekadex/Lekadex/Lekadex.Core/DoctorManager.cs
@@ -31,5 +31,70 @@ namespace Lekadex.Core
             }
             return _DoctorsMapper.Map(doctorEntities);
         }
+
+        public IEnumerable<PrescriptionDto> GetAllPrescriptionsForADoctor(int doctorId, string filter)
+        {
+            var prescriptionsEntities = _PrescriptionRepository.GetAllPrescriptions().Where(x => x.DoctorId == doctorId); //Trzeba jeszcze zawęźić te doctor ID's
+            if (string.IsNullOrEmpty(filter))
+            {
+                prescriptionsEntities = prescriptionsEntities.Where(x => x.Name.Contains(filter));
+            }
+            return _DoctorsMapper.Map(prescriptionsEntities); //Są tam też inne mappery
+        }
+
+        public IEnumerable<MedicineDto> GetAllMedicinesForAPrescription(int prescriptionId, string filter)
+        {
+            var medicinesEntities = _MedicineRepostiory.GetAllMedicines().Where(x => x.PrescriptionId == prescriptionId); //Trzeba jeszcze zawęźić te doctor ID's
+            if (string.IsNullOrEmpty(filter))
+            {
+                medicinesEntities = medicinesEntities.Where(x => x.ActiveSubstance.Contains(filter) || x.Name.Contains(filter) || x.CompanyName.Contains(filter));
+            }
+            return _DoctorsMapper.Map(medicinesEntities); //Są tam też inne mappery
+        }
+
+        public void AddNewMedicine(MedicineDto medicine, int prescriptionId)
+        {
+            var entity = _DoctorsMapper.Map(medicine);
+
+            entity.PrescriptionId = prescriptionId;
+
+            _MedicineRepostiory.AddNew(entity);
+        }
+
+        public void AddNewPrescription(PrescriptionDto prescription, int doctorsId)
+        {
+            var entity = _DoctorsMapper.Map(prescription);
+
+            entity.DoctorId = doctorsId;
+
+            _PrescriptionRepository.AddNew(entity);
+        }
+
+        public void AddNewPrescription(DoctorDto doctor)
+        {
+            var entity = _DoctorsMapper.Map(doctor);
+
+            _DoctorRepostiory.AddNew(entity);
+        }
+
+        public bool DeleteMedicine(MedicineDto medicine)
+        {
+            var entity = _DoctorsMapper.Map(medicine);
+
+            return _MedicineRepostiory.Delete(entity);
+        }
+
+        public bool DeletePrescription(PrescriptionDto prescription)
+        {
+            var entity = _DoctorsMapper.Map(prescription);
+            return _PrescriptionRepository.Delete(entity);
+        }
+
+        public void DeletePrescription(DoctorDto doctor)
+        {
+            var entity = _DoctorsMapper.Map(doctor);
+
+            _DoctorRepostiory.Delete(entity);
+        }
     }
 }
