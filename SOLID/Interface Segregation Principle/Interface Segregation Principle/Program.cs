471dd1e8e12c8168ac0c0aa30fd2fda9bcdd1d86
Kurczak1233 <kurczak12333@gmail.com>
2021-03-03 22:50:25 +0100

    Decorator + Interface segrtegation principle

---
+++ SOLID/Interface Segregation Principle/Interface Segregation Principle/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Interface_Segregation_Principle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Document
+    {
+
+    }
+    public interface IMachine : IPrinter, IScan
+    {
+    }
+
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+    public interface ICopy
+    {
+        void Scan(Document d);
+    }
+    public interface IScan
+    {
+        void Fax(Document d);
+    }
+    public class MultiFunctionPrinter : IMachine
+    {
+        private IPrinter printer;
+        private IScan scaner;
+
+        public MultiFunctionPrinter(IScan scaner, IPrinter print)
+        {
+            this.scaner = scaner;
+            this.printer = print;
+        }
+
+        //Decorator!
+        public void Print(Document d)
+        {
+            printer.Print(d);
+        }
+        public void Scan(Document d)
+        {
+            scaner.Fax(d);
+        }
+    }
+    public class OldFashinonedPrinter : ICopy
+    {
+        public void Scan(Document d)
+        {
+            //
+        }
+    }
+}
