6af75d8c0c9d4afef72dcc1f72092daef6e93a70
Kurczak1233 <kurczak12333@gmail.com>
2021-03-03 19:02:37 +0100

    Implement filter using open-close principle

--- SOLID/Open-Close Principle/Open-Close Principle/Program.cs
+++ SOLID/Open-Close Principle/Open-Close Principle/Program.cs
@@ -26,6 +26,7 @@ namespace Open_Close_Principle
 
     }
 
+    // Bad way to implement filter
     public class ProductFilter
     {
         public IEnumerable<Product> FilterBySize(Size size,IEnumerable<Product> produtcts)
@@ -49,13 +50,60 @@ namespace Open_Close_Principle
                 }
             }
         }
+
+        public IEnumerable<Product> FilterBySizeAndColor(Color color, Size size, IEnumerable<Product> produtcts)
+        {
+            foreach (var p in produtcts)
+            {
+                if (p.Size == size && p.Color == color)
+                {
+                    yield return p;
+                }
+            }
+        }
     }
 
+    // Good way to implement filter (open-close)
+    public interface ISpecificaction<T>
+    {
+        bool IsSatisfied(T t);
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecificaction<T> spec);
+    }
+
+    public class ColorSpecification : ISpecificaction<Product>
+    {
+        private Color color;
+        public ColorSpecification(Color color)
+        {
+            this.color = color;
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Color == color;
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecificaction<Product> spec)
+        {
+            foreach (var item in items)
+            {
+                if (spec.IsSatisfied(item))
+                    yield return item;
+            }
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
+            //NOT CLEVER WAY TO IMPLEMENT FILTER!
             var apple = new Product("Apple", Color.Green, Size.Small);
             var tree = new Product("Tree", Color.Green, Size.Large);
             var house = new Product("House", Color.Blue, Size.Large);
@@ -65,6 +113,14 @@ namespace Open_Close_Principle
             foreach (var p in pf.FilterByColor(Color.Green, products))
             {
                 Console.WriteLine($"- {p.Name} is green");
+            }
+            //CLEVER WAY TO IMPLEMENT FILTER
+            var bf = new BetterFilter();
+            Console.WriteLine("Green product (new):");
+            foreach(var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+            {
+                Console.WriteLine($"- {p.Name} is green");
+
             }
         }
     }
