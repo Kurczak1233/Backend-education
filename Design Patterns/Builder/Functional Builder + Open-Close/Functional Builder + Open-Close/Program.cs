2eadaf0310d772fdbc580ee49d493e483c3f21ae
Kurczak1233 <kurczak12333@gmail.com>
2021-03-04 22:57:34 +0100

    Sealed meaning + create project

---
+++ Design Patterns/Builder/Functional Builder + Open-Close/Functional Builder + Open-Close/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Functional_Builder___Open_Close
+{
+    public class Person
+    {
+        public string Name, Positon;
+    }
+
+    public sealed class PersonBuilder //Sealed means that no one can inherit from this class
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
