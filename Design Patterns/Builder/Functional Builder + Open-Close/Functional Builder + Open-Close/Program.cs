5cc644dfb3302112a046bb79ae2689dce0477ae1
Kurczak1233 <kurczak12333@gmail.com>
2021-03-04 23:23:13 +0100

    Resusable builder

--- Design Patterns/Builder/Functional Builder + Open-Close/Functional Builder + Open-Close/Program.cs
+++ Design Patterns/Builder/Functional Builder + Open-Close/Functional Builder + Open-Close/Program.cs
@@ -9,23 +9,45 @@ namespace Functional_Builder___Open_Close
         public string Name, Positon;
     }
 
-    public sealed class PersonBuilder //Sealed means that no one can inherit from this class
+    public abstract class FunctionalBuilder<TSubject, TSelf> where TSelf : FunctionalBuilder<TSubject, TSelf> where TSubject : new()
     {
-        private readonly List<Func<Person, Person>> actions = new List<Func<Person,Person>>();
-        public PersonBuilder Called(string name) => Do(p => p.Name = name);
+        private readonly List<Func<Person, Person>> actions = new List<Func<Person, Person>>();
 
         public Person Build() => actions.Aggregate(new Person(), (p, f) => f(p));
-        public PersonBuilder Do(Action<Person> action) => AddAction(action);
-        private PersonBuilder AddAction(Action<Person> action)
+        public TSelf Do(Action<Person> action) => AddAction(action);
+        private TSelf AddAction(Action<Person> action)
         {
             actions.Add(p =>
             {
                 action(p);
                 return p;
             });
-            return this;
+            return (TSelf)this;
         }
     }
+
+    public sealed class PersonBuilder : FunctionalBuilder<Person, PersonBuilder>
+    {
+        public PersonBuilder Called(string name) => Do(p => p.Name = name)
+    }
+
+    //public sealed class PersonBuilder //Sealed means that no one can inherit from this class
+    //{
+    //    private readonly List<Func<Person, Person>> actions = new List<Func<Person,Person>>();
+    //    public PersonBuilder Called(string name) => Do(p => p.Name = name);
+
+    //    public Person Build() => actions.Aggregate(new Person(), (p, f) => f(p));
+    //    public PersonBuilder Do(Action<Person> action) => AddAction(action);
+    //    private PersonBuilder AddAction(Action<Person> action)
+    //    {
+    //        actions.Add(p =>
+    //        {
+    //            action(p);
+    //            return p;
+    //        });
+    //        return this;
+    //    }
+    //}
 
     public static class PersonBuilderExtentions
     {
