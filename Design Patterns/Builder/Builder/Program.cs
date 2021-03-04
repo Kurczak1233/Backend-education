5fac5943901f7d120e8ea1b1a7016b70039cf34e
Kurczak1233 <kurczak12333@gmail.com>
2021-03-04 18:57:31 +0100

    Fluent builder

--- Design Patterns/Builder/Builder/Program.cs
+++ Design Patterns/Builder/Builder/Program.cs
@@ -56,10 +56,11 @@ namespace Builder
             this.rootName = rootName;
         }
 
-        public void AddChild(string childName, string childText)
+        public HtmlBuilder AddChild(string childName, string childText)
         {
             var e = new HtmlElement(childName, childText);
             root.Elements.Add(e);
+            return this;
         }
         public override string ToString()
         {
