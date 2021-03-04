f77fe8d5763c9aa06e637919236af27f936ccf67
Kurczak1233 <kurczak12333@gmail.com>
2021-03-04 23:08:32 +0100

    Builded builder

--- Design Patterns/Builder/Functional Builder + Open-Close/Functional Builder + Open-Close/Program.cs
+++ Design Patterns/Builder/Functional Builder + Open-Close/Functional Builder + Open-Close/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Functional_Builder___Open_Close
 {
@@ -9,13 +11,31 @@ namespace Functional_Builder___Open_Close
 
     public sealed class PersonBuilder //Sealed means that no one can inherit from this class
     {
+        private readonly List<Func<Person, Person>> actions = new List<Func<Person,Person>>();
+        public PersonBuilder Called(string name) => Do(p => p.Name = name);
 
+        public Person Build() => actions.Aggregate(new Person(), (p, f) => f(p));
+        public PersonBuilder Do(Action<Person> action) => AddAction(action);
+        private PersonBuilder AddAction(Action<Person> action)
+        {
+            actions.Add(p =>
+            {
+                action(p);
+                return p;
+            });
+            return this;
+        }
+    }
+
+    public static class PersonBuilderExtentions
+    {
+        public static PersonBuilder WorksAs(this PersonBuilder builder, string postion) => builder.Do(p => p.Positon = postion);
     }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var Person = new PersonBuilder().Called("Michal").WorksAs("Developer").Build();
         }
     }
 }
