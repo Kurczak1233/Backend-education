45e840b63d498314798d19eadb75eaf3406ea828
Kurczak1233 <kurczak12333@gmail.com>
2021-03-30 16:03:55 +0200

    Actual progress

--- Genetic Alghoritm/NSGA-III/NSGA-III/Program.cs
+++ Genetic Alghoritm/NSGA-III/NSGA-III/Program.cs
@@ -6,7 +6,19 @@ namespace NSGA_III
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //1. Calculate number reference points(H).
+            //2. Generate initial population.
+            //3. Realize non-dominated population sorting
+            //4. Select P1 and P2
+            //5. Crossover between P1 and P2
+            //6. Realize non-dominated population sorting
+            //7. Normalize the population members
+            //8. Associate population with reference points
+            //9. Apply the niche preservation
+            //10. Keep niche obtained solutions for next generation
+            //11. If iter>Iter_max (NO --> 4., YES -->12.)
+            //12. Display results
+            //13. Stop.
         }
     }
 }
