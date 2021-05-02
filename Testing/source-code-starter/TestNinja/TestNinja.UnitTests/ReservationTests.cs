c6256aa621027e868ea8532f8d047b3cfe45b3bc
Michał Kupczak <kurczak12333@gmail.com>
2021-05-02 21:37:51 +0200

    Create folder for tests

---
+++ Testing/source-code-starter/TestNinja/TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserAdmin_ReturnsTrue()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert 
+        }
+    }
+}
