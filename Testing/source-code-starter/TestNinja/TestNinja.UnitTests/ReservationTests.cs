17c1fd1ee6215e62cc7eb92de92f371d2175885e
Michał Kupczak <kurczak12333@gmail.com>
2021-05-02 22:14:42 +0200

    Created tests

--- Testing/source-code-starter/TestNinja/TestNinja.UnitTests/ReservationTests.cs
+++ Testing/source-code-starter/TestNinja/TestNinja.UnitTests/ReservationTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
@@ -9,10 +10,37 @@ namespace TestNinja.UnitTests
         public void CanBeCancelledBy_UserAdmin_ReturnsTrue()
         {
             //Arrange
-
+            Reservation reservation = new();
             //Act
-
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
             //Assert 
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+
+        public void CanBeCancelledBy_Creator_ReturnsTrue()
+        {
+            //Arrange
+            User user = new();
+            Reservation reservation = new()
+            {
+                MadeBy = user
+            };
+            //Act
+            var result = reservation.CanBeCancelledBy(user);
+            //Assert 
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+
+        public void CanBeCancelledBy_OrdinaryUser_ReturnsFalse()
+        {
+            //Arrange
+            Reservation reservation = new();
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { });
+            //Assert 
+            Assert.IsFalse(result);
         }
     }
 }
